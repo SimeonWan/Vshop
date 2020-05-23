657375eed0e895870985b40b6a2197bcf465f59d
lipengxiangEdward <lipengxiangedward@gmail.com>
2020-05-23 19:54:07 +0800

    实现通用接口模块：Vshop-common

---
+++ API/Vshop-common/src/main/java/com/Ace/Vshop/common/exception/ApiException.java
@@ -0,0 +1,31 @@
+package com.Ace.Vshop.common.exception;
+
+import com.Ace.Vshop.common.api.IErrorCode;
+
+/***
+ * 自定义API异常
+ */
+public class ApiException extends RuntimeException {
+    private IErrorCode errorCode;
+
+    public ApiException(IErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ApiException(String message) {
+        super(message);
+    }
+
+    public ApiException(Throwable cause) {
+        super(cause);
+    }
+
+    public ApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
---
+++ API/Vshop-common/src/main/java/com/Ace/Vshop/common/exception/Asserts.java
@@ -0,0 +1,18 @@
+package com.Ace.Vshop.common.exception;
+
+import com.Ace.Vshop.common.api.IErrorCode;
+
+/***
+ * 断言处理类
+ *
+ * 用于抛出各种API异常
+ */
+public class Asserts {
+    public static void fail(String message) {
+        throw new ApiException(message);
+    }
+
+    public static void fail(IErrorCode errorCode) {
+        throw new ApiException(errorCode);
+    }
+}
