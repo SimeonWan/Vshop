d0baf4c3199afc3bfb6a00ac493fe787b1fcd46c
lipengxiangEdward <lipengxiangedward@gmail.com>
2020-05-28 20:19:04 +0800

    MBG

---
+++ shop/shop-mbg/src/main/java/com/lpx/shop/mapper/CmsMemberReportMapper.java
@@ -0,0 +1,22 @@
+package com.lpx.shop.mapper;
+
+import com.lpx.shop.model.CmsMemberReport;
+import com.lpx.shop.model.CmsMemberReportExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CmsMemberReportMapper {
+    long countByExample(CmsMemberReportExample example);
+
+    int deleteByExample(CmsMemberReportExample example);
+
+    int insert(CmsMemberReport record);
+
+    int insertSelective(CmsMemberReport record);
+
+    List<CmsMemberReport> selectByExample(CmsMemberReportExample example);
+
+    int updateByExampleSelective(@Param("record") CmsMemberReport record, @Param("example") CmsMemberReportExample example);
+
+    int updateByExample(@Param("record") CmsMemberReport record, @Param("example") CmsMemberReportExample example);
+}
